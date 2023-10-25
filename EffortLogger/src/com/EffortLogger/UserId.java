cd296708d55ff835fa88bbd77d5b913d7cc46a7b
Maguire <mwbrady1@asu.edu>
2023-10-24 21:36:56 -0700

    saving for now
    a bunch of userid interface stuff. switching it over to be an admin facing thing

--- EffortLogger/src/com/EffortLogger/UserId.java
+++ EffortLogger/src/com/EffortLogger/UserId.java
@@ -1,7 +1,66 @@
 package com.EffortLogger;
 
-public class UserId {
-	public UserId() {
-		
-	}
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class UserId extends Application {
+	 public void start(Stage primaryStage) {
+		 Button button = new Button("Hello World!");
+		 GridPane gp = new GridPane();
+		 Pane filler1 = new Pane();
+		 Pane filler2 = new Pane();
+		 Pane filler3 = new Pane();
+		 Pane filler4 = new Pane();
+		 Label userLabel, statusLabel, passLabel;
+		 userLabel = new Label("User ID: ");
+		 statusLabel = new Label("Please enter User ID and Password");
+		 passLabel = new Label("Password: ");
+		 BorderPane bp = new BorderPane(); 
+		 TextField userTB = new TextField();
+		 PasswordField passTB =new PasswordField();
+		 userTB.setPromptText("User ID");
+		 passTB.setPromptText("Password");
+		 BorderPane passwordField = new BorderPane();
+		 BorderPane userField = new BorderPane();
+		 filler1.setMinSize(240, 120);
+		 filler2.setMinSize(240, 120);
+		 filler3.setMinSize(240, 120);
+		 filler4.setMinSize(240, 120);
+		 bp.setMinSize(240, 6);
+		 gp.setMinWidth(240);
+		 gp.add(filler1, 0, 0);
+		 gp.add(filler2, 1, 0);
+		 gp.add(filler3, 2, 0);
+		 gp.add(filler4, 1, 2);
+		 userField.setLeft(userLabel);
+		 userField.setRight(userTB);
+		 passwordField.setLeft(passLabel);
+		 passwordField.setRight(passTB);
+		 bp.setTop(statusLabel);
+		 bp.setCenter(userField);
+		 bp.setBottom(passwordField);
+		 gp.add(bp, 1, 1);
+		 Scene scene = new Scene(gp, 720,480);
+		 primaryStage.setScene(scene);
+		 primaryStage.show();
+	 }
+	 public static void main(String[] args) {
+		 launch(args);
+	 }
+	 private class ButtonHandler implements EventHandler<ActionEvent>{
+		 public void handle(ActionEvent e) {
+			 //get userID from db
+			 //
+		 }
+	 }
 }
--- EffortLogger/src/module-info.java
+++ EffortLogger/src/module-info.java
@@ -1,3 +1,8 @@
 module EffortLogger {
+	requires java.desktop;
 	requires javafx.graphics;
+	requires javafx.controls;
+	requires javafx.fxml;
+	opens com.EffortLogger to javafx.fxml;
+	exports com.EffortLogger;
 }
